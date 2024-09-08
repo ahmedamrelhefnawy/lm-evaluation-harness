93b59d64867adf559f08ddff2c26e484a997f42d
Ahmed_Adel <109729603+ahmeda335@users.noreply.github.com>
2024-09-08 10:02:18 +0300

    Update evaluator.py
    fixing an error

--- lm_eval/evaluator.py
+++ lm_eval/evaluator.py
@@ -241,12 +241,13 @@ def simple_evaluate(
             + ".db",
         )
 
-    # if task_manager is None:    
-    #     task_manager = TaskManager(verbosity)
-
-    # task_dict = get_task_dict(tasks, task_manager)
+    if task_dict == "":
+        if task_manager is None:    
+            task_manager = TaskManager(verbosity)
     
-    task_dict = task_dict
+        task_dict = get_task_dict(tasks, task_manager)
+    else:
+        task_dict = task_dict
 
     # helper function to recursively apply config overrides to leaf subtasks, skipping their constituent groups.
     # (setting of num_fewshot ; bypassing metric calculation ; setting fewshot seed)
